7106e7d1ab1ef62beaaa73883c81950fd4e9ef6a
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2024-05-25 15:35:41 +0200

    Add bubble sort implementation

---
+++ Algorithms/Sorting/Bubble-Sort/BubbleSort.java
@@ -0,0 +1,23 @@
+public class BubbleSortExample {
+    public static void bubleSort(int[] array, int n) {
+        int i, j, temp;
+        boolean swapped;
+
+        for (i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
