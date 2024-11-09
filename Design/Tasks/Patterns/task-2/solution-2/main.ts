3e0a8d136e80e15e706ed83424da424ab58ec05c
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2024-11-09 12:19:53 +0100

    Implement main file for solution task-2

---
+++ Design/Tasks/Patterns/task-2/solution-2/main.ts
@@ -0,0 +1,33 @@
+const MAX_GROUP_TOTAL = 2000;
+
+const MOCKED_API_FOR_EXAMPLE = {
+    host: 'example.com',
+    path: '/api/latest.json?id=',
+    key: 'exampleofkey',
+}
+
+const basket = {
+    Devices: [
+        { name: 'Laptop', price: 1500 },
+        { name: 'Mouse', price: 100 },
+        { name: 'Keyboard', price: 100 },
+    ],
+    Textile: [
+        { name: 'Bag', price: 50 },
+    ]
+}
+
+const main = async () => {
+    try {
+        // TODO should be implemented
+        const validatedGroup = validateGroup(MAX_GROUP_TOTAL);
+        const bill = calculateTotal(basket, validatedGroup);
+        const openedExchange = await new Exchange(MOCKED_API_FOR_EXAMPLE);
+        const amount = openedExchange.convert(bill.total, 'USD');
+        console.log({ amount })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main();
