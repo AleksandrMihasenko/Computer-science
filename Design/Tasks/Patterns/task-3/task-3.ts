661bfb6c275b9613d4828ed7a383bdd3d8143aea
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2024-11-10 14:41:58 +0100

    Add task 3

---
+++ Design/Tasks/Patterns/task-3/task-3.ts
@@ -0,0 +1,4 @@
+// Tasks for rewriting:
+//   - implement EventTarget class
+//   - It should allow to register event listeners, remove listeners, and dispatch events to the registered listeners.
+//   - Use native JavaScript techniques
