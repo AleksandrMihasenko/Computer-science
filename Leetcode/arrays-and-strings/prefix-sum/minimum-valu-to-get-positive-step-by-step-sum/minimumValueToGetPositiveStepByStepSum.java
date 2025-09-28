ff17fd4402970e54930c77db506aa7217c3a4fd4
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2025-09-28 12:25:38 +0200

    Add solutions for leetcode problem

---
+++ Leetcode/arrays-and-strings/prefix-sum/minimum-valu-to-get-positive-step-by-step-sum/minimumValueToGetPositiveStepByStepSum.java
@@ -0,0 +1,11 @@
+public int minStartValue(int[] nums) {
+    int min = 0;
+    int total = 0;
+
+    for (int num : nums) {
+        total += num;
+        min = Math.min(min, total);
+    }
+
+    return 1 - min;
+}
