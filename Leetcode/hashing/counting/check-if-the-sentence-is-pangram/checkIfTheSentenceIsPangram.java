398c082b561668e2f5b8c609ebe5aae3b56926a5
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2025-10-19 13:29:25 +0200

    Add solutions for leetcode problem

---
+++ Leetcode/hashing/counting/check-if-the-sentence-is-pangram/checkIfTheSentenceIsPangram.java
@@ -0,0 +1,8 @@
+public boolean checkIfPangram(String sentence) {
+    Set<Character> seen = new HashSet<>();
+
+    for (char currChar : sentence.toCharArray())
+        seen.add(currChar);
+
+    return seen.size() == 26;
+}
