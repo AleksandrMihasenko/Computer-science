baf076a06029da48da8dcc06483701f296e178c5
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2025-11-23 12:13:09 +0100

    Add solutions for leetcode problem

---
+++ Leetcode/hashing/existing/contains-duplicate/containsDuplicate.java
@@ -0,0 +1,12 @@
+public boolean containsDuplicate(int[] nums) {
+    Set<Integer> result = new HashSet<>();
+
+    for (int num : nums) {
+        if (result.contains(num)) {
+            return true;
+        }
+        result.add(num);
+    }
+
+    return false;
+}
